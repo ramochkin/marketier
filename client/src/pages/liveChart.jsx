58b5068f45faefa8c825427036086e96965100dd
Vy Nguyen <vy1872@gmail.com>
2022-04-01 10:12:28 -0500

    deleted the banner

--- client/src/pages/liveChart.jsx
+++ client/src/pages/liveChart.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 
 import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
-import WelcomeBanner from '../partials/liveChart/WelcomeBanner';
 import LiveChart from '../partials/liveChart/liveChart';
 
 function Dashboard() {
@@ -23,9 +22,6 @@ function Dashboard() {
 
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-            {/* Welcome banner */}
-            <WelcomeBanner />
-
             {/* Cards */}
             <div className="grid grid-cols-12 gap-6">
               {/* Line chart (Real Time Value) */}
