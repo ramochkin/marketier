ddbea08227ac5edf4453b9eb4e47df9f8b524d04
llting592 <llting592@gmail.com>
2022-04-04 14:51:07 -0400

    added linechart for symbol page

---
+++ client/src/components/partials/LineChart.js
@@ -0,0 +1,55 @@
+import { Chart as ChartJS, registerables } from 'chart.js';
+import { Line } from 'react-chartjs-2'
+ChartJS.register(...registerables);
+
+function LineChart({data}) {
+    var data = {
+      labels: data?.map(x => x.name),
+      datasets: [{
+        label: `${data?.length} data Available`,
+        data: data?.map(x => x.price),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    };
+  
+    var options = {
+      maintainAspectRatio: false,
+      scales: {
+      },
+      legend: {
+        labels: {
+          fontSize: 50,
+        },
+      },
+    }
+  
+    return (
+      <div>
+        <Line
+          data={data}
+          height={600}
+          width={800}
+          options={options}
+  
+        />
+      </div>
+    )
+  }
+  
+  export default LineChart
