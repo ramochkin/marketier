aa9e590b0aa69c050e9a0bf8322e842dadef8f1f
Yujen Chen <yujengogo@gmail.com>
2022-04-04 15:09:38 -0400

    logo implement

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import LoginForm from '../LoginForm'
 import SignUpForm from '../SignUpForm'
 import auth from '../../utils/auth'
 import SearchBar from '../SearchBar/SearchBar'
-
+import logo from '../../assets/image/marketier.png'
 
 export default function Navbar() {
     const [toggleMenu, setToggleMenu] = useState(false);
@@ -37,58 +37,64 @@ export default function Navbar() {
         <>
             <nav>
                 {(toggleMenu || screenWidth > 500) && (
-                    <ul className="navbarLinks">
-                        <li className="leftLink" ><Link to="/">Home</Link></li>
-                        <li className="leftLink" ><Link to="/news">News</Link></li>
-                        <li className="leftLink" ><Link to="/stocks">Stocks</Link></li>
-                        <li className="leftLink" ><Link to="/currencies">Currencies</Link></li>
-                        <li className="leftLink" ><Link to="/crypto">Cryptocurrencies</Link></li>
-                        {auth.loggedIn() ? (
-                            <>
-                                <li className="leftLink" ><Link to="/profile">Profile</Link></li>
-                                <li className="leftlink" ><Link to='#' onClick={() => auth.logout()}>Log Out</Link></li>
+                    <div className='NavflexRow'>
+                         <img src={logo} alt="Logo" width="75" height="75" href="/" />
 
-                            </>
-                        )
+                        <ul className="navbarLinks">
+                            <li className="leftLink" ><Link to="/">Home</Link></li>
+                            <li className="leftLink" ><Link to="/news">News</Link></li>
+                            <li className="leftLink" ><Link to="/stocks">Stocks</Link></li>
+                            <li className="leftLink" ><Link to="/currencies">Currencies</Link></li>
+                            <li className="leftLink" ><Link to="/crypto">Cryptocurrencies</Link></li>
+                            {auth.loggedIn() ? (
+                                <>
+                                    <li className="leftLink" ><Link to="/profile">Profile</Link></li>
+                                    <li className="leftlink" ><Link to='#' onClick={() => auth.logout()}>Log Out</Link></li>
+
+                                </>
+                        
+                )
                             : (<li className="leftlink" ><Link to='#' onClick={() => setShowModal(true)}>Login/Sign Up</Link></li>)}
-                        <SearchBar />
-                    </ul>
-                )}
+                <SearchBar />
+            </ul>
+        </div>
+    )
+}
 
 
-                <button onClick={toggleNav} className="arrowbtn">▼</button>
-            </nav>
-            <Modal
-                size='lg'
-                show={showModal}
-                onHide={() => setShowModal(false)}
-                aria-labelledby='signup-modal'>
-                {/* tab container to do either signup or login component */}
-                <Tab.Container defaultActiveKey='login'>
-                    <Modal.Header closeButton>
-                        <Modal.Title id='signup-modal'>
-                            <Nav variant='pills'>
-                                <Nav.Item>
-                                    <Nav.Link eventKey='login'>Login</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Tab.Content>
-                            <Tab.Pane eventKey='login'>
-                                <LoginForm handleModalClose={() => setShowModal(false)} />
-                            </Tab.Pane>
-                            <Tab.Pane eventKey='signup'>
-                                <SignUpForm handleModalClose={() => setShowModal(false)} />
-                            </Tab.Pane>
-                        </Tab.Content>
-                    </Modal.Body>
-                </Tab.Container>
-            </Modal>
+<button onClick={toggleNav} className="arrowbtn">▼</button>
+            </nav >
+    <Modal
+        size='lg'
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby='signup-modal'>
+        {/* tab container to do either signup or login component */}
+        <Tab.Container defaultActiveKey='login'>
+            <Modal.Header closeButton>
+                <Modal.Title id='signup-modal'>
+                    <Nav variant='pills'>
+                        <Nav.Item>
+                            <Nav.Link eventKey='login'>Login</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Tab.Content>
+                    <Tab.Pane eventKey='login'>
+                        <LoginForm handleModalClose={() => setShowModal(false)} />
+                    </Tab.Pane>
+                    <Tab.Pane eventKey='signup'>
+                        <SignUpForm handleModalClose={() => setShowModal(false)} />
+                    </Tab.Pane>
+                </Tab.Content>
+            </Modal.Body>
+        </Tab.Container>
+    </Modal>
         </>
     )
 }
